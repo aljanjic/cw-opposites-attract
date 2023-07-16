1c18250596bcda493b3e3db2d800a8891077f2d6
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-16 23:03:12 +0200

    TS solution + JS refactor

--- src/oppositesAttract.js
+++ src/oppositesAttract.js
@@ -1,5 +1,10 @@
 function oppositesAttract(fl1, fl2) {
-  return (fl1 + fl2) % 2 !== 0 ? true : false;
+  return (fl1 + fl2) % 2 !== 0;
 }
+
+
+// export function lovefunc(flower1: number, flower2: number): boolean {
+//   return Math.abs(flower1 - flower2) % 2 !== 0;
+// }
 
 module.exports = oppositesAttract;
